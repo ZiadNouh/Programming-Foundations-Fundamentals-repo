b9aa954d2d296f41672a93241a85c61db2791cc4
ZiadNouh <ziad.nouh111@gmail.com>
2022-05-15 05:06:24 +0200

    First commit

---
+++ Exercise Files/Chap04/my_ans_ch4.py
@@ -0,0 +1,8 @@
+fave_food = input("What's my favorite food?")
+
+if fave_food == "chicken":
+    print("Yup! So amazing")
+else:
+    print("Yuck! that's not it!")
+
+print("Thanks for playing")
---
+++ Exercise Files/Chap05/my_ans_ch5.py
@@ -0,0 +1,8 @@
+def favorite_city(name) :
+    print("One of my favorite cities is ", name)
+
+favorite_city("Alexandria")  
+favorite_city("Alexandria")
+favorite_city("Alexandria")
+
+
